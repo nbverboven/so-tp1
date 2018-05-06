aedd7a68b240a82cd5a39bcf443ea7279f97a488
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-05-05 21:02:29 -0300

    Reentrega - separé el código para tomar tiempos en 3 archivos

--- entregable/grafico.py
+++ entregable/grafico.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 datos = np.loadtxt("tiempos.csv", delimiter=',')
+datos2 = np.loadtxt("tiempos2.csv", delimiter=',')
+datos3 = np.loadtxt("tiempos3.csv", delimiter=',')
 
 plt.plot(datos[:,0], datos[:,1], 'o', label="maximum1")
-plt.plot(datos[:,0], datos[:,2], 's', label="maximum2")
-plt.plot(datos[:,0], datos[:,3], '^', label="maximum3")
+plt.plot(datos[:,0], datos2[:,1], 's', label="maximum2")
+plt.plot(datos[:,0], datos3[:,1], '^', label="maximum3")
 
 plt.ylim(ymin=0, ymax=27)
-plt.xlim(xmin=0, xmax=31)
+plt.xlim(xmin=0, xmax=11)
 plt.ylabel("Tiempo de ejecución (ms)")
 plt.xlabel("Cantidad de threads")
 # plt.legend()
